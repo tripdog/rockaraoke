870b0b06750dd588f9d05d1baed7ca9fd325455a
Tom Phillips <eflips@yahoo.com>
2021-06-22 17:34:01 -0700

    Add hamburger menu change wording on nav

--- js/scripts.js
+++ js/scripts.js
@@ -31,18 +31,18 @@
     offset: 75
   });
 
-  // // Collapse Navbar
-  // var navbarCollapse = function() {
-  //   if ($("#mainNav").offset().top > 100) {
-  //     $("#mainNav").addClass("navbar-scrolled");
-  //   } else {
-  //     $("#mainNav").removeClass("navbar-scrolled");
-  //   }
-  // };
-  // // Collapse now if page is not at top
-  // navbarCollapse();
-  // // Collapse the navbar when page is scrolled
-  // $(window).scroll(navbarCollapse);
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-scrolled");
+    } else {
+      $("#mainNav").removeClass("navbar-scrolled");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
 
   // Magnific popup calls
   $('#portfolio').magnificPopup({
